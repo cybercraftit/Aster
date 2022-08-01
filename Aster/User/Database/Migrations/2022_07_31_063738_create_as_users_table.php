42a9be623813d4c0496bf88cb662b44a52df96bc
Mithu <cemithu06@gmail.com>
2022-08-01 15:45:45 +0600

    make user_email unique field and added timestamp

--- Aster/User/Database/Migrations/2022_07_31_063738_create_as_users_table.php
+++ Aster/User/Database/Migrations/2022_07_31_063738_create_as_users_table.php
@@ -18,12 +18,13 @@ class CreateAsUsersTable extends Migration
             $table->string('user_login', 60)->default('')->index('user_login_key');
             $table->string('user_pass')->default('');
             $table->string('user_nicename', 50)->default('')->index('user_nicename');
-            $table->string('user_email', 100)->default('')->index('user_email');
+            $table->string('user_email', 100)->unique()->default('')->index('user_email');
             $table->string('user_url', 100)->default('');
             $table->dateTime('user_registered')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->string('user_activation_key')->default('');
             $table->integer('user_status')->default(0);
             $table->string('display_name', 250)->default('');
+            $table->timestamps();
         });
     }
 
