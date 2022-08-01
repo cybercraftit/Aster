44d33556e6d614ea6638f6825bf733af1d2a424f
Mithu <cemithu06@gmail.com>
2022-08-01 21:01:49 +0600

    Custom login post route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\AsLoginController;
 use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,5 +20,6 @@ Route::get('/', function () {
 });
 
 Auth::routes();
+Route::post('login', [AsLoginController::class, 'loginAttempt']);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
