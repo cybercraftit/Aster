8c0a46e8cf264b6cb866983ae0fdcaa39eea33bd
Mithu <cemithu06@gmail.com>
2022-08-03 14:59:23 +0600

    intall unocss/vite and configure

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import Unocss from '@unocss/vite'
 
 export default defineConfig({
     plugins: [
+        Unocss({ /* options */ }),
         laravel({
             input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
+                /*'resources/sass/app.scss',
+                'resources/js/app.js',*/
+                'Aster/Admin/Resources/assets/sass/admin.scss',
+                'Aster/Admin/Resources/assets/js/app.js'
             ],
             refresh: true,
         }),
