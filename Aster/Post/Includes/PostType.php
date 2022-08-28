511ace5ab6b37f1cad8ba95cb6f5c04011f88bce
Mithu <cemithu06@gmail.com>
2022-08-28 15:31:01 +0600

    PostType class to contain common methods

---
+++ Aster/Post/Includes/PostType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Aster\Post\Includes;
+
+class PostType{
+
+    /**
+     * Instance
+     *
+     * @since 1.0.0
+     *
+     * @access private
+     * @static
+     */
+    private static $_instance = null;
+
+    protected $post_types = [];
+
+    /**
+     * Instance
+     *
+     * Ensures only one instance of the class is loaded or can be loaded.
+     *
+     * @since 1.0.0
+     *
+     * @access public
+     * @static
+     *
+     * @return ${ClassName} An instance of the class.
+     */
+    public static function instance() {
+
+        if ( is_null( self::$_instance ) ) {
+            self::$_instance = new self();
+        }
+        return self::$_instance;
+
+    }
+
+    public function __construct() {
+
+    }
+
+    public function register_post_type( $post_type, $args = [] ) {
+        $this->post_types[$post_type] = $args;
+    }
+}
