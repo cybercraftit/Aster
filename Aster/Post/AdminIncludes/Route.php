593e49807269e053f2299ea3ae51cf4fd53ca50c
Mithu <cemithu06@gmail.com>
2022-09-04 17:40:59 +0600

    Route methods

---
+++ Aster/Post/AdminIncludes/Route.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Aster\Post\AdminIncludes;
+
+class Route{
+
+    /**
+     * Instance
+     *
+     * @since 1.0.0
+     *
+     * @access private
+     * @static
+     */
+    private static $_instance = null;
+
+    protected $routes = [];
+
+    /**
+     * Instance
+     *
+     * Ensures only one instance of the class is loaded or can be loaded.
+     *
+     * @since 1.0.0
+     *
+     * @access public
+     * @static
+     *
+     * @return ${ClassName} An instance of the class.
+     */
+    public static function instance() {
+
+        if ( is_null( self::$_instance ) ) {
+            self::$_instance = new self();
+        }
+        return self::$_instance;
+
+    }
+
+    public function __construct() {
+
+    }
+
+    public function add_route( $slug, $callback, $name = null ) {
+        if ( !$name ) {
+            $name = $slug;
+        }
+
+        $this->routes[$name] = [
+            'callback' => $callback,
+            'name' => $name,
+            'slug' => $slug
+        ];
+    }
+
+    public function add_model_route( $slug, $callback, $model, $context = 'browse', $admin = false ) {
+        $name = $this->get_model_route_name( $model, $context, $admin );
+        $this->routes[$name] = [
+            'callback' => $callback,
+            'name' => $name,
+            'slug' => $slug
+        ];
+    }
+
+    public function get_model_route_name ( $model, $context = 'browse', $action_method, $admin = false ) {
+        $name = ( $admin ? 'admin.' : '' ) . $model . '.' . $context;
+        if ( $action_method != 'get' ) {
+            $name .= '.' . $action_method;
+        }
+        return $name;
+    }
+
+    public function get_model_route( $model, $context = 'browse', $args = [], $admin = false ) {
+        $name = $this->get_model_route_name( $model, $context, $admin );
+        return$this->routes[$name];
+    }
+}
