ca28f244abc975ba030c6a98d8bc9014525bf8d5
Mithu <cemithu06@gmail.com>
2022-10-10 15:36:21 +0600

    Rules added to form

--- packages/Cybercraftit/Aster/src/Modules/Post/Forms/PostForm.php
+++ packages/Cybercraftit/Aster/src/Modules/Post/Forms/PostForm.php
@@ -9,10 +9,34 @@ class PostForm extends Form
     public function buildForm()
     {
         $this
-            ->add('post_title', 'text' )
+            ->add('post_title', 'text',
+                [
+                    'label' => 'Post title',
+                    'rules' => 'required|min:5',
+                    'error_messages' => [
+                        'post_title.required' => 'The title field is mandatory.'
+                    ]
+                ]
+            )
 //            ->add('post_name', 'text' )
-            ->add('post_content', 'textarea' )
-            ->add( 'post_excerpt', 'textarea' )
+            ->add('post_content', 'textarea',
+                [
+                    'label' => 'Post content',
+                    'rules' => 'required|max:5000',
+                    'error_messages' => [
+                        'post_content.required' => 'The content field is mandatory.'
+                    ]
+                ]
+            )
+            ->add( 'post_excerpt', 'textarea',
+                [
+                    'label' => 'Post excerpt',
+                    'rules' => 'required|max:1000',
+                    'error_messages' => [
+                        'post_excerpt.required' => 'The excerpt field is mandatory.'
+                    ]
+                ]
+            )
             ->add('post_status', 'select', [
                 'choices' => ['draft' => 'Draft', 'publish' => 'Publish', 'pending' => 'Pending Review', 'private' => 'Private', 'trash' => 'Trash'],
                 'selected' => 'draft',
