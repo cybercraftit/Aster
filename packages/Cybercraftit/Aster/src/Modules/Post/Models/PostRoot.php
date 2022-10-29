04d46e5b438b40fccabfe9ee3aff6073d9a28f72
Mithu <cemithu06@gmail.com>
2022-10-29 12:19:37 +0600

    PostRool model added

---
+++ packages/Cybercraftit/Aster/src/Modules/Post/Models/PostRoot.php
@@ -0,0 +1,27 @@
+<?php
+namespace Cybercraftit\Aster\Modules\Post\Models;
+
+use Cybercraftit\Aster\Modules\User\Models\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PostRoot extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'post_title',
+        'post_content',
+        'post_excerpt',
+        'post_status',
+        'comment_status'
+    ];
+    protected $table = 'posts';
+    protected $primaryKey = 'ID';
+    public $timestamps = false;
+
+    public function author() {
+        return $this->belongsTo( User::class, 'post_author', 'ID' );
+    }
+
+}
