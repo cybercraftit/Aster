cf1f2d66bb4f99eab23d10c328eaca7aac1d5001
Mithu <cemithu06@gmail.com>
2022-11-11 11:26:08 +0600

    Post type Model improved

--- packages/Cybercraftit/Aster/src/Modules/Post/Models/Page.php
+++ packages/Cybercraftit/Aster/src/Modules/Post/Models/Page.php
@@ -5,21 +5,5 @@ use Illuminate\Database\Eloquent\Builder;
 
 class Page extends PostRoot
 {
-    /**
-     * The "booting" method of the model.
-     *
-     * @return void
-     */
-    protected static function boot()
-    {
-        parent::boot();
-
-        static::addGlobalScope('post_type', function (Builder $builder) {
-            $builder->where('post_type', 'page' );
-        });
-
-        self::creating(function($model) {
-            $model->post_type = 'page';
-        });
-    }
+    protected static $post_type = 'post';
 }
--- packages/Cybercraftit/Aster/src/Modules/Post/Models/PostRoot.php
+++ packages/Cybercraftit/Aster/src/Modules/Post/Models/PostRoot.php
@@ -2,6 +2,7 @@
 namespace Cybercraftit\Aster\Modules\Post\Models;
 
 use Cybercraftit\Aster\Modules\User\Models\User;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -19,9 +20,28 @@ class PostRoot extends Model
     protected $table = 'posts';
     protected $primaryKey = 'ID';
     public $timestamps = false;
+    protected static $post_type = 'post';
 
     public function author() {
         return $this->belongsTo( User::class, 'post_author', 'ID' );
+    }
+
+    /**
+     * The "booting" method of the model.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope('post_type', function (Builder $builder) {
+            $builder->where('post_type', get_called_class()::$post_type );
+        });
+
+        self::creating(function($model) {
+            $model->post_type = 'page';
+        });
     }
 
 }
